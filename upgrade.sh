6c51b7ff4c1abe4db453cb898d5f2bd3a8d2cb8c
disintegr8te <44122555+disintegr8te@users.noreply.github.com>
2022-04-05 11:50:29 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 # check if the reboot flag file exists. 
-# We created this file before rebooting.
 if [ ! -f /var/run/resume-after-reboot_dist ]; then
   echo "running script for the first time.."
-  
 uname -a
 lsb_release -a
-sudo visudo -f /etc/sudoers.d/do-release-upgrade.
 sudo apt update -y
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 sudo apt install update-manager-core -y
 sudo sync
 
-  # Preparation for reboot
-  script="bash /upgrade.sh"
+# Preparation for reboot
+script="bash /upgrade.sh"
   
-  # add this script to .bashrcso it gets triggered immediately after reboot
-  echo "$script" >> ~/.bachrc
+# add this script to .bashrc so it gets triggered immediately after reboot
+echo "$script" >> ~/.bachrc
   
-    # create a flag file to check if we are resuming from reboot.
+# create a flag file to check if we are resuming from reboot.
   sudo touch /var/run/resume-after-reboot_dist
   
   echo "rebooting.."
