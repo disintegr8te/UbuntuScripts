3e7c79f284a01e6f2c5e4344f4021c84a8444107
disintegr8te <44122555+disintegr8te@users.noreply.github.com>
2022-04-05 12:15:28 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,36 +3,32 @@
 if [ ! -f /var/run/resume-after-reboot_dist1 ]; then
   echo "running script for the first time.."
   
-#Get Release
-lsb_release -a
-
-#Update Package Sources
-sudo apt update -y
-
-#Set Flags for Unattended Installation and Removal of Packages and do a dist-upgrade do get the latest packages for that release.
-export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
-
-#Install the Update Manager if not present
-sudo apt install update-manager-core -y
-
-#Sync Write Buffer before reboot
-sudo sync
-
-# Preparation for reboot
-script="bash /upgrade.sh"
+  #Get Release
+  lsb_release -a
   
-# add this script to .bashrc so it gets triggered immediately after reboot
-echo "$script" >> ~/.bachrc
+  #Update Package Sources
+  sudo apt update -y
+
+  #Set Flags for Unattended Installation and Removal of Packages and do a dist-upgrade do get the latest packages for that release.
+  export DEBIAN_FRONTEND=noninteractive
+  sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
+
+  #Install the Update Manager if not present
+  sudo apt install update-manager-core -y
+
+  #Sync Write Buffer before reboot
+  sudo sync
   
-# create a flag file to check if we are resuming from reboot.
+  #create a flag file to check if we are resuming from reboot.
   sudo touch /var/run/resume-after-reboot_dist1
  
 #reboot 1. Time
   echo "rebooting.."
   sudo reboot
-    
-else 
+fi 
+
+
+if [-f /var/run/resume-after-reboot_dist1]; then
   echo "resuming script after reboot.."
   
   # Remove the line that we added in bashrc
@@ -47,3 +43,4 @@ else
   sudo apt update -y
   lsb_release -a
 fi
+
